a199bf5ce1e256f2af0e0867ad912f9d622934b4
kota122a <kuataic@gmail.com>
2023-11-27 15:14:03 +0900

    スクリプトの改良と自分のところにシーン追加

--- Assets/Students/202503_02/Scripts/Back Manager.cs
+++ Assets/Students/202503_02/Scripts/Back Manager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class BackManager : MonoBehaviour
 {
-    public Sprite[] backgrounds; // 複数の背景を保持するための配列
+    public int selectedBackgroundIndex = 0; // 選択された背景のインデックス
 
     void OnEnable()
     {
@@ -19,15 +19,41 @@ public class BackManager : MonoBehaviour
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // シーンが読み込まれた後に実行されるコード
-        int backgroundIndex = scene.buildIndex; // シーンのビルドインデックスを取得して背景を選択
+        SetBackground(selectedBackgroundIndex);
+    }
 
-        // 背景が設定されている場合、それを表示する
-        if (backgroundIndex < backgrounds.Length && backgrounds[backgroundIndex] != null)
+    // 外部から背景を変更するためのメソッド
+    public void ChangeBackgroundByIndex(int index)
+    {
+        selectedBackgroundIndex = index;
+        SetBackground(selectedBackgroundIndex);
+    }
+
+    // 選択された背景を設定するメソッド
+    void SetBackground(int index)
+    {
+        GameObject backgroundObject = GameObject.Find("GlobalInstance/GlobalSpriteAnchor/BackGround"); // バックグラウンドを含むオブジェクトの名前を指定
+
+        if (backgroundObject != null)
         {
-            // 例えば、背景を表示するためのコードをここに追加する
-            // この例では、SpriteRendererを使って背景を表示すると仮定しています
-            GetComponent<SpriteRenderer>().sprite = backgrounds[backgroundIndex];
+            SpriteRenderer spriteRenderer = backgroundObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                // backgrounds配列から背景を取得する代わりに、インデックスに基づいて設定
+                // 例えば、backgrounds配列が使えない場合は、直接スプライトを割り当てることもできます
+                // spriteRenderer.sprite = 背景のスプライト;
+
+                // 以下は仮の例です。スプライトを直接割り当てる場合は、適切なスプライトを設定してください。
+                spriteRenderer.color = Color.white; // 仮の背景の色を赤に設定
+            }
+            else
+            {
+                Debug.LogWarning("SpriteRendererがアタッチされていません。");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("指定された背景オブジェクトが見つかりません。");
         }
     }
 }
