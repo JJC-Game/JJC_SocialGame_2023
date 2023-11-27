271e76560018bb5130242aaa76f23f0fab597919
morita1205 <morisin1205@gmail.com>
2023-11-27 13:32:43 +0900

    ガチャエフェクトの作成

---
+++ Assets/Students/202603_11/Scripts/Rainbow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rainbow : MonoBehaviour
+{
+    public float duration = 5.0F;   //色が変わるタイミング(時間)
+    ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    [System.Obsolete]
+
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitube = Mathf.Cos(phi) * 0.5F + 0.5F;
+        ps.startColor = Color.HSVToRGB(amplitube, 1, 1);
+    }
+}
