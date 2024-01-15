62d06c93922f1832dba35b4d9cd83f862379c48c
jjc-a05 <jjc.joetsu.computer.a05@gmail.com>
2024-01-15 11:26:21 +0900

    0115sample

---
+++ Assets/Students/201503_01/Scripts/UIManager_0115.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager_0115 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string charaName = Application.fixDataManager.GetCharaName(1);
+        Debug.Log(charaName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
