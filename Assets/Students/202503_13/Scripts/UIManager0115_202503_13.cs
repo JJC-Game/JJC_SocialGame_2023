ccd0eb9526ebebab731d8863c5d61b9471a3a6da
vally7469 <128009138+vally7469@users.noreply.github.com>
2024-01-18 09:40:40 +0900

    自分のシーンの作成

---
+++ Assets/Students/202503_13/Scripts/UIManager0115_202503_13.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager0115_202503_13 : MonoBehaviour
+{
+    public int CharaID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Application.fixDataManager.GetCharaName(CharaID));
+
+        /*Debug.Log(BattleData.BattleFixData,);
+        Application.FixDataManager.GetBattleData battleData;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
